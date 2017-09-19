a6f8f34e6ced952daabcce8dfae49a0b111204bc
comkar999 <31873952+comkar999@users.noreply.github.com>
2017-09-19 01:27:17 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StudentAssignment;
+
+namespace StudentAssignment
+{
+    public partial class Form1 : Form
+    {
+        List<Student> studentsList = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+           
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            studentsList.Add(new Student() { FirstName = textBox1.Text, LastName = textBox2.Text, Address = textBox3.Text, Age = textBox4.Text, Major = textBox4.Text });
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = studentsList;
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAssignment
+{
+  public  class Student
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Age { get; set; }
+        public string Major { get; set; }
+
+
+
+    }
+}
